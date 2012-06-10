06ea4affce1d8788ea1657936eebac1f5bebf645
dragonvirgo <norihide.shimatani@gmail.com>
2012-06-10 17:14:34 +0900

    リファクタリング２回目
    Signed-off-by: dragonvirgo <norihide.shimatani@gmail.com>

--- PaintApplication/PaintApplication/src/paint/PaintApplication/PaintApplicationActivity.java
+++ PaintApplication/PaintApplication/src/paint/PaintApplication/PaintApplicationActivity.java
@@ -3,59 +3,36 @@ package paint.PaintApplication;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
-import android.content.DialogInterface.OnClickListener;
+import android.content.res.TypedArray;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-public class PaintApplicationActivity extends Activity implements View.OnClickListener {
+public class PaintApplicationActivity extends Activity {
 
-	// 
 	private final int INTENT_FOR_CONFIG_VIEW = 1;
 	private final int INTENT_FOR_PAINT_APPLICATION_THICK = 2;
-	
 	private PaintView paintView;
-	ImageView ivUndo;
-	ImageView ivRedo;
 
 	@Override public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);// メインにカスタムビューを追加
 		paintView = (PaintView)findViewById(R.id.paintView);
-
-		ImageView ivBrush = (ImageView) findViewById(R.id.imageView_brush);
-		ImageView ivColor = (ImageView) findViewById(R.id.imageView_color);
-		ImageView ivEraser = (ImageView) findViewById(R.id.imageView_eraser);
-		ivUndo = (ImageView) findViewById(R.id.imageView_undo);
-		ivRedo = (ImageView) findViewById(R.id.imageView_redo);
-
-		ivBrush.setOnClickListener(this);
-		ivColor.setOnClickListener(this);
-		ivEraser.setOnClickListener(this);
-		ivUndo.setOnClickListener(this);
-		ivRedo.setOnClickListener(this);
-
-		ivUndo.setEnabled(false);
-		ivUndo.setAlpha(128);
-		ivRedo.setEnabled(false);
-		ivRedo.setAlpha(128);
+		new SubMenuClickListener(); // サブメニューボタンに関しては全てこのリスナーに任せる。anonymousなオブジェクトでよい。
 	}
 	@Override public boolean onCreateOptionsMenu(Menu menu) {
 		super.onCreateOptionsMenu(menu);
 		getMenuInflater().inflate(R.menu.menu, menu);
 		return true;
 	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
+	@Override public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
 		case R.id.configId: // 設定ボタン押下時
 			Intent it = new Intent(getApplicationContext(), ConfigView.class);
@@ -88,22 +65,17 @@ public class PaintApplicationActivity extends Activity implements View.OnClickLi
 		return true;
 	}
 	public void onActivityResult(int reqcode, int result, Intent it) {
-		// 太さも設定画面からの戻り処理
 		switch(reqcode) {
-		case INTENT_FOR_PAINT_APPLICATION_THICK:
+		case INTENT_FOR_PAINT_APPLICATION_THICK: // 太さ設定画面からの戻り処理
 			if (result == RESULT_OK) {
-				int ft = it.getIntExtra("THICK", 2);
-				paintView.setThick(ft);
-				boolean aa = it.getBooleanExtra("ANTIALIAS", true);
-				paintView.setAntiAlias(aa);
+				paintView.setThickness(it.getIntExtra("THICK", 2));
+				paintView.setAntiAlias(it.getBooleanExtra("ANTIALIAS", true));
 			}
 			break;
 		case INTENT_FOR_CONFIG_VIEW:
-			int c = ConfigView.getBgColor(this); // 背景色変更処理
-			paintView.setBackgroundColor(c);// 背景色変更処理
-			paintView.bgColor =  c;
-			paintView.mode = ConfigView.getStamp(this);
-			paintView.bgmFlag = ConfigView.getSound(this);
+			paintView.setBgColor(ConfigView.getBgColor(this));
+			paintView.setBgmMode(ConfigView.getSound(this));
+			paintView.setElementMode(ConfigView.getStamp(this));
 			SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
 			SharedPreferences.Editor editor = prefs.edit();
 			editor.clear(); 
@@ -111,32 +83,39 @@ public class PaintApplicationActivity extends Activity implements View.OnClickLi
 			break;
 		}
 	}
-	// ***********サブメニュークリックイベント*********************************
-	public void onClick(View v) {
-		switch (v.getId()) {
-		case R.id.imageView_brush:
-			paintView.undoFlag = false;
-			Intent intent = new Intent(this, PaintApplicationThick.class);
-			intent.putExtra("THICK", paintView.getThick());
-			intent.putExtra("COLOR", paintView.getColor());
-			intent.putExtra("ANTIALIAS", paintView.isAntiAlias());
-			startActivityForResult(intent, INTENT_FOR_PAINT_APPLICATION_THICK);
-			break;
-		case R.id.imageView_color:
-			ColorPickerDialog dlg;
-			dlg = new ColorPickerDialog(this, new ColorPickerDialog.OnColorChangedListener() {
-				public void colorChanged(int color) {
-					paintView.setColor(color);
-				}
-			}, Color.WHITE);
-			dlg.show();
-			Toast.makeText(getApplicationContext(), Integer.toHexString(paintView.getColor()),Toast.LENGTH_SHORT).show();
-			break;
-		case R.id.imageView_eraser:
-			paintView.eraserMode = !paintView.eraserMode;
-			break;
-		case R.id.imageView_undo: paintView.historyBack(); break;
-		case R.id.imageView_redo: paintView.historyForward(); break;
+	// サブメニューボタンのリスナー
+	private class SubMenuClickListener implements View.OnClickListener {
+		private SubMenuClickListener() {
+			TypedArray tArray = getResources().obtainTypedArray(R.array.subMenuButtons);
+			for (int i=0; i<tArray.length(); i++) {
+				int resourceId = tArray.getResourceId(i, 0);
+				ImageView imageView = (ImageView)findViewById(resourceId);
+				imageView.setOnClickListener(this);
+			}
+		}
+		public void onClick(View v) {
+			switch (v.getId()) {
+			case R.id.imageView_brush:
+				Intent intent = new Intent(PaintApplicationActivity.this, PaintApplicationThick.class);
+				intent.putExtra("THICK", paintView.getThickness());
+				intent.putExtra("COLOR", paintView.getBrushColor());
+				intent.putExtra("ANTIALIAS", paintView.isAntiAlias());
+				startActivityForResult(intent, INTENT_FOR_PAINT_APPLICATION_THICK);
+				break;
+			case R.id.imageView_color:
+				ColorPickerDialog dlg;
+				dlg = new ColorPickerDialog(PaintApplicationActivity.this, new ColorPickerDialog.OnColorChangedListener() {
+					public void colorChanged(int color) {
+						paintView.setBrushColor(color);
+					}
+				}, Color.WHITE);
+				dlg.show();
+				Toast.makeText(getApplicationContext(), Integer.toHexString(paintView.getBrushColor()),Toast.LENGTH_SHORT).show();
+				break;
+			case R.id.imageView_eraser:	paintView.toggleEraserMode();	break;
+			case R.id.imageView_undo:	paintView.historyBack();		break;
+			case R.id.imageView_redo:	paintView.historyForward();		break;
+			}
 		}
 	}
 }
