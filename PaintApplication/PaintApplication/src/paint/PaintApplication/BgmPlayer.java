968e4089c572ac0fe730a87672a2f06ee3beb412
trial-dragonvirgo <ernesto.janne@gmail.com>
2012-06-11 10:30:00 +0900

    githubからmasterを取り込んだだけ。

---
+++ PaintApplication/PaintApplication/src/paint/PaintApplication/BgmPlayer.java
@@ -0,0 +1,39 @@
+package paint.PaintApplication;
+
+import java.util.Random;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.media.MediaPlayer;
+import android.util.Log;
+
+public class BgmPlayer {
+	private static final String TAG = BgmPlayer.class.getSimpleName();
+	
+	private Context context;
+	private MediaPlayer mediaPlayer;
+	private int[] bgmIds;
+	private Random rand = new Random();
+	
+	BgmPlayer(Context c) {
+		context = c;
+		TypedArray ar = context.getResources().obtainTypedArray(R.array.bgms);
+		bgmIds = new int[ar.length()];
+		for (int i=0; i<ar.length(); i++) bgmIds[i] = ar.getResourceId(i, 0);
+	}
+	void start() {
+		int index = rand.nextInt(bgmIds.length);
+		mediaPlayer = MediaPlayer.create(context, bgmIds[index]);
+		mediaPlayer.setLooping(true);
+		mediaPlayer.start();
+	}
+	void stop() {
+		try {
+			mediaPlayer.stop();
+		} catch (Exception e) {
+			Log.e(TAG, "メディアプレーヤーのstopに失敗しました。", e);
+		} finally {
+			mediaPlayer.release();
+		}
+	}
+}
--- PaintApplication/PaintApplication/src/paint/PaintApplication/PaintView.java
+++ PaintApplication/PaintApplication/src/paint/PaintApplication/PaintView.java
@@ -5,6 +5,7 @@ import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.Date;
 
+import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.CompressFormat;
@@ -23,6 +24,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 public class PaintView extends View {
+	// 描画エレメントの形状
 	private final int MODE_LINE = -1;
 	private final int MODE_STAMP_TRIANGLE = 0;
 	private final int MODE_STAMP_RECTANGLE = 1;
@@ -32,181 +34,114 @@ public class PaintView extends View {
 	private final int MODE_STAMP_RECTANGLE_DURATION = 5;
 	private final int MODE_STAMP_CIRCLE_DURATION = 6;
 	private final int MODE_STAMP_STAR_DURATION = 7;
+	// Moveイベント発生判定の許容値
+	private final int TOLERANCE = 3;
 	
+	// 描画エレメント
 	private class Element {
-		public Path path = new Path(); 			// path情報を保持
-		public Paint paint = new Paint(); 		// paint情報を保持
-		boolean eraser =  eraserMode;
+		private Path path = new Path(); 						// path情報を保持
+		private Paint paint = new Paint(); 						// paint情報を保持
+		private boolean eraser =  eraserMode;					// 消しゴムかどうか
 		private Element(){
-			paint.setColor(eraserMode? bgColor : color); // アンチエイリアスの有無
-			paint.setAntiAlias(antiAlias); // アンチエイリアスの有無
-			paint.setStyle(Paint.Style.STROKE); // 線のスタイル（STROKE：図形の輪郭線のみ表示、FILL:塗る）
-			paint.setStrokeWidth(thick); // 線の太さ
-			paint.setStrokeCap(Paint.Cap.ROUND); // 　線の先端スタイル（ROUND：丸くする）
-			paint.setStrokeJoin(Paint.Join.ROUND); // 線と線の接続点のスタイル（ROUND：丸くする）
+			paint.setColor(eraserMode? bgColor : brushColor);	// 描画エレメントの色
+			paint.setAntiAlias(antiAlias);						// アンチエイリアスの有無
+			paint.setStyle(Paint.Style.STROKE);					// 線のスタイル（STROKE：図形の輪郭線のみ表示、FILL:塗る）
+			paint.setStrokeWidth(thickness);					// 線の太さ
+			paint.setStrokeCap(Paint.Cap.ROUND);				// 　線の先端スタイル（ROUND：丸くする）
+			paint.setStrokeJoin(Paint.Join.ROUND);				// 線と線の接続点のスタイル（ROUND：丸くする）
 		}
 	}
-	
-	int mode = MODE_LINE;
-	boolean bgmFlag = true;
-	private float oldX = 0f; // ひとつ前のX座標保持
-	private float oldY = 0f; // ひとつ前のY座標保持
-	
-	private ArrayList<Element> elements = new ArrayList<Element>(); // 全てのパス情報を保持
-	private Element element = null; // 線情報クラスのインスタンス
-//	private Element workingElement = null; // 線情報クラスのインスタンス
-	
-	private int undo = 0; // アンドゥ処理のためのカウント変数
-	boolean undoFlag = true;	// 再描画バグのテストフラグ
+	private Element element = null;			// 描画エレメント
+	private ArrayList<Element> elements = new ArrayList<Element>();
+	// イベント位置
+	private PointF oldPos  = new PointF();	// 前回のイベント位置
+	private PointF newPos  = new PointF();	// 今回のイベント位置
+	private PointF downPos = new PointF();	// ACTION_DOWNのイベント位置
+	// 各種モード
+	private int elementMode = MODE_LINE;	// 描画エレメントの形状
+	private boolean eraserMode = false; 	// 消しゴムモード
+	private boolean bgmMode = true;			// 効果音出力モード
+	// 各種属性
+	private int bgColor = Color.BLACK;		// 背景色
+	private int brushColor = Color.WHITE;	// ブラシの色
+	private int thickness = 2;				// ブラシの太さ
+	private boolean antiAlias = true;		// ブラシのアンチエイリアス
+	// Undo, Redo, 関連
+	private int undo = 0;					// アンドゥ処理のためのカウント変数
+	private boolean undoFlag = true;		// 再描画バグのテストフラグ
+	private ImageView ivUndo;				// Undoボタン
+	private ImageView ivRedo;				// Rndoボタン
+	// その他
+	private BgmPlayer bgmPlayer;			// 効果音出力オブジェクト
+	private MediaScannerConnection mc;		// メディアスキャナへのコネクタ
 
-	private int color = Color.WHITE;
-	int bgColor = Color.BLACK;
-	private int thick = 2; // 線の太さ
-	boolean eraserMode = false; // 消しゴムモード
-	private boolean antiAlias = true;	// アンチエイリアス
-
-	private MediaScannerConnection mc; // メディアスキャン
-	private onBgm onbgm = new onBgm();
-	private MediaPlayer mp = null; // BGM用
-	private static Context _context = null; 
-
-	PaintApplicationActivity paintAA;
-	
-	public PaintView(Context context) {
-		this(context, null);
-	}
+	public PaintView(Context context) { this(context, null); }
 	public PaintView(Context context, AttributeSet attrs) {
 		super(context, attrs);
-		paintAA = (PaintApplicationActivity)getContext();
-		undo = 0;
+		bgmPlayer = new BgmPlayer(getContext());
 	}
-	public void onDraw(Canvas canvas) {
-		if (element == null) { // 線が無いときは描画しない
-			return;
-		}
-		for (int i = 0; i < elements.size() + undo; i++) {
-			Path pt = elements.get(i).path;
-			Paint pa = elements.get(i).paint;
-			canvas.drawPath(pt, pa);
-		}
-		if (undoFlag) { 
-			canvas.drawPath(element.path, element.paint);
+	// setter & getter methods
+	void toggleEraserMode()			{ eraserMode = !eraserMode; }
+	void setElementMode(int mode)	{ elementMode = mode; }
+	void setBgmMode(boolean mode)	{ bgmMode = mode; }
+	void setBgColor(int c)			{ setBackgroundColor(bgColor = c); }
+	void setBrushColor(int c)		{ brushColor = c; }
+	void setThickness(int t)		{ thickness = t; }
+	void setAntiAlias(boolean a)	{ antiAlias = a; }
+	int getBrushColor()				{ return brushColor; }
+	int getThickness()				{ return thickness; }
+	boolean isAntiAlias()			{ return antiAlias; }
+	// ビューのレイアウト確定時
+	@Override public void onLayout(boolean changed, int left, int top, int right, int bottom) {
+		Activity parent = (Activity)getContext();
+		ivUndo = (ImageView)parent.findViewById(R.id.imageView_undo);
+		ivRedo = (ImageView)parent.findViewById(R.id.imageView_redo);
+		setButtonEnabled(ivUndo, false);
+		setButtonEnabled(ivRedo, false);
+	}
+	@Override public void onDraw(Canvas canvas) {
+		if (element != null) { // カレントの描画エレメントが無いときは描画しない
+			for (int i=0; i<elements.size() + undo; i++) {
+				Element e = elements.get(i);
+				canvas.drawPath(e.path, e.paint);
+			}
+			if (undoFlag) { 
+				canvas.drawPath(element.path, element.paint);
+			}
 		}
 	}
 	public boolean onTouchEvent(MotionEvent e) {
-		// タッチイベント判定処理
+		newPos.x = e.getX();
+		newPos.y = e.getY();
 		switch (e.getAction()) {
 		case MotionEvent.ACTION_DOWN: // タッチして画面を押した時
 			element = new Element();
-
-			oldX = e.getX();
-			oldY = e.getY();
-			element.path.moveTo(oldX, oldY);
-
-			onbgm.onBgmran();
-		    mp.setLooping(true);
-			mp.start();
+			element.path.moveTo(newPos.x, newPos.y);
+			downPos.x = newPos.x;
+			downPos.y = newPos.y;
+			
+			if (bgmMode) bgmPlayer.start();
 			break;
-
 		case MotionEvent.ACTION_MOVE: // タッチしてから離すまでの移動して間
 			undoFlag = true;
-			float sq = (float)Math.sqrt((e.getX() - oldX)*(e.getX() - oldX)+(e.getY() - oldY)*(e.getY() - oldY));
-			switch (mode) {
+			switch (elementMode) {
 			case MODE_LINE:
-				// 滑らかモード
-				int TOLERANCE = 6;
-				if (Math.abs(e.getX() - oldX) >= TOLERANCE || Math.abs(e.getY() - oldY) >= TOLERANCE) {
-					element.path.quadTo(oldX, oldY, (oldX + e.getX()) / 2, (oldY + e.getY()) / 2);
+				// 移動距離が許容値以下の場合は描画しない
+				if (Math.abs(newPos.x - oldPos.x) >= TOLERANCE || Math.abs(newPos.y - oldPos.y) >= TOLERANCE) {
+					// 滑らかな線を描画する
+					element.path.quadTo(oldPos.x, oldPos.y, (oldPos.x + newPos.x)/2, (oldPos.y + newPos.y)/2);
 				}
-				oldX = e.getX(); oldY = e.getY();
-				break;
-				// △　スタンプ
-			case MODE_STAMP_TRIANGLE:
-				element.path.reset();
-				element.path.moveTo(oldX, oldY-50f*(sq/50));
-				element.path.lineTo(oldX-50f*(sq/50), oldY+20f*(sq/50));
-				element.path.lineTo(oldX+50f*(sq/50), oldY+20f*(sq/50));
-				element.path.lineTo(oldX, oldY-50f*(sq/50));		
-				break;
-			// ???@?X?^???v
-			case MODE_STAMP_RECTANGLE:
-				element.path.reset();
-				element.path.moveTo(oldX-50f*(sq/50), oldY-50f*(sq/50));
-				element.path.lineTo(oldX+50f*(sq/50), oldY-50f*(sq/50));
-				element.path.lineTo(oldX+50f*(sq/50), oldY+50f*(sq/50));
-				element.path.lineTo(oldX-50f*(sq/50), oldY+50f*(sq/50));
-				element.path.lineTo(oldX-50f*(sq/50), oldY-50f*(sq/50));
-				break;
-			// ???@?X?^???v
-			case MODE_STAMP_CIRCLE:
-				element.path.reset();
-				element.path.addCircle(oldX, oldY, 50f*(sq/50), Direction.CW);	
-				break;
-			// ???@?X?^???v
-			case MODE_STAMP_STAR:
-				element.path.reset();
-				float theta = (float)(Math.PI * 72 / 180);
-				float r = 50f;
-				PointF center = new PointF(oldX, oldY);
-				float dx1 = (float)(r*Math.sin(theta));
-				float dx2 = (float)(r*Math.sin(2*theta));
-				float dy1 = (float)(r*Math.cos(theta));
-				float dy2 = (float)(r*Math.cos(2*theta));
-				element.path.moveTo(center.x, center.y-r*(sq/50));
-				element.path.lineTo(center.x-dx2*(sq/50), center.y-dy2*(sq/50));
-				element.path.lineTo(center.x+dx1*(sq/50), center.y-dy1*(sq/50));
-				element.path.lineTo(center.x-dx1*(sq/50), center.y-dy1*(sq/50));
-				element.path.lineTo(center.x+dx2*(sq/50), center.y-dy2*(sq/50));
-				element.path.lineTo(center.x, center.y-r*(sq/50));
-				break;
-				
-			case MODE_STAMP_TRIANGLE_DURATION:
-				element.path.moveTo(oldX, oldY-50f*(sq/50));
-				element.path.lineTo(oldX-50f*(sq/50), oldY+20f*(sq/50));
-				element.path.lineTo(oldX+50f*(sq/50), oldY+20f*(sq/50));
-				element.path.lineTo(oldX, oldY-50f*(sq/50));		
-				break;
-			// ???@?X?^???v
-			case MODE_STAMP_RECTANGLE_DURATION:
-				element.path.moveTo(oldX-50f*(sq/50), oldY-50f*(sq/50));
-				element.path.lineTo(oldX+50f*(sq/50), oldY-50f*(sq/50));
-				element.path.lineTo(oldX+50f*(sq/50), oldY+50f*(sq/50));
-				element.path.lineTo(oldX-50f*(sq/50), oldY+50f*(sq/50));
-				element.path.lineTo(oldX-50f*(sq/50), oldY-50f*(sq/50));
-				break;
-			// ???@?X?^???v
-			case MODE_STAMP_CIRCLE_DURATION:
-				element.path.addCircle(oldX, oldY, 50f*(sq/50), Direction.CW);	
-				break;
-			// ???@?X?^???v
-			case MODE_STAMP_STAR_DURATION:
-				theta = (float)(Math.PI * 72 / 180);
-				r = 50f;
-				center = new PointF(oldX, oldY);
-				dx1 = (float)(r*Math.sin(theta));
-				dx2 = (float)(r*Math.sin(2*theta));
-				dy1 = (float)(r*Math.cos(theta));
-				dy2 = (float)(r*Math.cos(2*theta));
-				element.path.moveTo(center.x, center.y-r*(sq/50));
-				element.path.lineTo(center.x-dx2*(sq/50), center.y-dy2*(sq/50));
-				element.path.lineTo(center.x+dx1*(sq/50), center.y-dy1*(sq/50));
-				element.path.lineTo(center.x-dx1*(sq/50), center.y-dy1*(sq/50));
-				element.path.lineTo(center.x+dx2*(sq/50), center.y-dy2*(sq/50));
-				element.path.lineTo(center.x, center.y-r*(sq/50));
 				break;
 			default:
+				drawStamp(elementMode, element, downPos, newPos);
 				break;
 			}
 			invalidate();			
 			break;
 		case MotionEvent.ACTION_UP: // タッチして離した時
-			switch (mode) {
+			switch (elementMode) {
 			case MODE_LINE:
-				oldX = e.getX();
-				oldY = e.getY();
-				element.path.lineTo(oldX, oldY);
-				break;
-			default:
+				element.path.lineTo(newPos.x, newPos.y);
 				break;
 			}
 			// UNDOの後に新しい書き込みがされた際の、古い履歴オブジェクトの削除を行う
@@ -215,61 +150,91 @@ public class PaintView extends View {
 				undo++;
 			}
 			elements.add(element);
-//			mode = MODE_LINE;
 			invalidate();
 			
-			paintAA.ivUndo = (ImageView) paintAA.findViewById(R.id.imageView_undo);
-			paintAA.ivRedo = (ImageView) paintAA.findViewById(R.id.imageView_redo);
-			setButtonEnabled(paintAA.ivUndo,true);
-			setButtonEnabled(paintAA.ivRedo,false);
-
-			try {
-				mp.stop();
-			} catch (Exception er) {
-			} finally {
-				mp.release();
-			}
+			setButtonEnabled(ivUndo,true);
+			setButtonEnabled(ivRedo,false);
+			if (bgmMode) bgmPlayer.stop();
 			break;
 		default:
 			break;
 		}
+		oldPos.x = newPos.x;
+		oldPos.y = newPos.y;
 		return true;
 	}
-
 	// 1操作戻る
 	public void historyBack() {
 		undo--;
-		setButtonEnabled(paintAA.ivRedo,true);
-		if (elements.size() + undo == 0) {
-			setButtonEnabled(paintAA.ivUndo,false);
-		}
 		undoFlag = false;
+		setButtonEnabled(ivRedo, true);
+		if (elements.size() + undo == 0) setButtonEnabled(ivUndo, false);
 		invalidate();
 	}
-
 	// 1操作進む
 	public void historyForward() {
 		undo++;
-		setButtonEnabled(paintAA.ivUndo,true);
-		if (undo == 0) {
-			setButtonEnabled(paintAA.ivRedo,false);
-		}
 		undoFlag = false;
+		setButtonEnabled(ivUndo, true);
+		if (undo == 0) setButtonEnabled(ivRedo, false);
 		invalidate();
 	}
-
 	// クリア
 	public void clearPathList() {
+		undo = 0;
 		elements.clear();
 		element = null;
-		undo = 0;
-		oldX = 0f;
-		oldY = 0f;
-		setButtonEnabled(paintAA.ivUndo,false);
-		setButtonEnabled(paintAA.ivRedo,false);
 		invalidate();
+		setButtonEnabled(ivUndo, false);
+		setButtonEnabled(ivRedo, false);
 	}
-
+	// Undo, Redoボタンのenable/disable処理
+	private void setButtonEnabled(ImageView v, boolean enable){
+		v.setEnabled(enable);
+		v.setAlpha(enable? 255 : 128);
+	}
+	// スタンプ図形を描画する。 (m:mode, e:element, o:old position, n:new position)
+	private void drawStamp(int m, Element e, PointF o, PointF n) {
+		float sq = (float)Math.sqrt((n.x - o.x)*(n.x - o.x)+(n.y - o.y)*(n.y - o.y));
+		switch (m) {
+		case MODE_STAMP_TRIANGLE:	e.path.reset(); // 敢てbreakしない。
+		case MODE_STAMP_TRIANGLE_DURATION:
+			e.path.moveTo(o.x, o.y-sq);
+			e.path.lineTo(o.x-sq, o.y+sq);
+			e.path.lineTo(o.x+sq, o.y+sq);
+			e.path.lineTo(o.x, o.y-sq);		
+			break;
+		case MODE_STAMP_RECTANGLE:	e.path.reset(); // 敢てbreakしない。
+		case MODE_STAMP_RECTANGLE_DURATION:
+			e.path.moveTo(o.x-sq, o.y-sq);
+			e.path.lineTo(o.x+sq, o.y-sq);
+			e.path.lineTo(o.x+sq, o.y+sq);
+			e.path.lineTo(o.x-sq, o.y+sq);
+			e.path.lineTo(o.x-sq, o.y-sq);
+			break;
+		case MODE_STAMP_CIRCLE:		e.path.reset(); // 敢てbreakしない。
+		case MODE_STAMP_CIRCLE_DURATION:
+			e.path.addCircle(o.x, o.y, sq, Direction.CW);	
+			break;
+		case MODE_STAMP_STAR:		e.path.reset(); // 敢てbreakしない。
+		case MODE_STAMP_STAR_DURATION:
+			float theta = (float)(Math.PI * 72 / 180);
+			float dx1 = (float)(Math.sin(theta));
+			float dx2 = (float)(Math.sin(2*theta));
+			float dy1 = (float)(Math.cos(theta));
+			float dy2 = (float)(Math.cos(2*theta));
+			PointF center = new PointF(o.x, o.y);
+			e.path.moveTo(center.x, center.y-sq);
+			e.path.lineTo(center.x-dx2*sq, center.y-dy2*sq);
+			e.path.lineTo(center.x+dx1*sq, center.y-dy1*sq);
+			e.path.lineTo(center.x-dx1*sq, center.y-dy1*sq);
+			e.path.lineTo(center.x+dx2*sq, center.y-dy2*sq);
+			e.path.lineTo(center.x, center.y-sq);
+			break;
+		default:
+			break;
+		}
+	}
 	// 画像ファイルを保存
 	public boolean isSaveToFile(PaintApplicationActivity paint) {
 		// キャッシュからキャプチャを作成、そのためキャッシュをON
@@ -280,23 +245,20 @@ public class PaintView extends View {
 		
 		// 保存先の決定(存在しない場合は作成)
 		File file;
-		String path = Environment.getExternalStorageDirectory()
-				+ "/PaintApplication/";
-		if (!Environment.getExternalStorageState().equals(
-				Environment.MEDIA_MOUNTED)) {
-			file = Environment.getDataDirectory();
-		} else {
+		String path = Environment.getExternalStorageDirectory() + "/PaintApplication/";
+		if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
 			file = new File(path);
 			file.mkdirs();
+		} else {
+			file = Environment.getDataDirectory();
 		}
-		// 一意となるファイル名を取得（タイムスタンプ）
 		Date d = new Date();
+		// 一意となるファイル名を取得（タイムスタンプ）
 		String fileName = String.format("%4d%02d%02d-%02d%02d%02d.png",
 				(1900 + d.getYear()), d.getMonth() + 1, d.getDate(),
 				d.getHours(), d.getMinutes(), d.getSeconds());
 		file = new File(path + fileName + ".png");
-		// 画像をファイルに書き込む
-		try {
+		try { // 画像をファイルに書き込む
 			FileOutputStream out = new FileOutputStream(file);
 			bitmap.compress(CompressFormat.PNG, 100, out);
 			out.flush();
@@ -307,90 +269,17 @@ public class PaintView extends View {
 			return false;
 		}
 	}
-
-	// アンドゥボタンEnabled処理メソッド
-	public static void setButtonEnabled(ImageView btn, boolean alpha){
-		if(alpha){
-			btn.setEnabled(true);
-			btn.setAlpha(255);
-		}
-		else{
-			btn.setEnabled(false);
-			btn.setAlpha(128);		
-		}
-	}
+	// メディアスキャナにスキャンさせる
 	void mediaScanExecute(PaintApplicationActivity paint, final String file) {
 		mc = new MediaScannerConnection(paint,
-				new MediaScannerConnection.MediaScannerConnectionClient() {
-					public void onMediaScannerConnected() {
-						mc.scanFile(file, "image/png");
-					}
-					public void onScanCompleted(String path, Uri uri) {
-						mc.disconnect();
-					}
-				});
-		mc.connect();
-	}
-
-	int getColor()				{ return color; }
-	void setColor(int c)		{ color = c; }
-	int getThick()				{ return thick; }
-	void setThick(int futosa)	{ thick = futosa; }
-	boolean isAntiAlias()		{ return antiAlias; }
-	void setAntiAlias(boolean aa){ antiAlias = aa; }
-	
-	public class onBgm {
-//		public MediaPlayer mp = null; // BGM用
-//		public boolean bgmFlag = true; // BGMflag用
-
-//		private Context getContext() {
-//
-//			return _context;
-//		}
-
-		public void onBgmran() {
-			if (bgmFlag == true) {
-				int ran = (int) (Math.random() * 10) + 1;
-				{
-					switch (ran) {
-					case 1: // BGM1呼び出し
-						mp = MediaPlayer.create(getContext(), R.raw.bgm1);
-						break;
-					case 2: // BGM2呼び出し
-						mp = MediaPlayer.create(getContext(), R.raw.bgm2);
-						break;
-					case 3: // BGM3呼び出し
-						mp = MediaPlayer.create(getContext(), R.raw.bgm3);
-						break;
-					case 4: // BGM4呼び出し
-						mp = MediaPlayer.create(getContext(), R.raw.bgm4);
-						break;
-					case 5: // BGM5呼び出し
-						mp = MediaPlayer.create(getContext(), R.raw.bgm5);
-						break;
-					case 6: // BGM6呼び出し
-						mp = MediaPlayer.create(getContext(), R.raw.bgm6);
-						break;
-					case 7: // BGM7呼び出し
-						mp = MediaPlayer.create(getContext(), R.raw.bgm7);
-						break;
-					case 8: // BGM8呼び出し
-						mp = MediaPlayer.create(getContext(), R.raw.bgm8);
-						break;
-					case 9: // BGM9呼び出し
-						mp = MediaPlayer.create(getContext(), R.raw.bgm9);
-						break;
-					case 10: // BGM10呼び出し
-						mp = MediaPlayer.create(getContext(), R.raw.bgm10);
-						break;
-					default:
-						break;
-					}
+			new MediaScannerConnection.MediaScannerConnectionClient() {
+				public void onMediaScannerConnected() {
+					mc.scanFile(file, "image/png");
 				}
-			} else {
-				mp = MediaPlayer.create(getContext(), R.raw.bgm0); // BGMなし
-			}
-		}
-		// }
+				public void onScanCompleted(String path, Uri uri) {
+					mc.disconnect();
+				}
+			});
+		mc.connect();
 	}
 }
